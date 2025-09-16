261cd3ddfc571c56d80cf9383ff2eab4f84c4bab
1MasonMuch <masonm126@nycstudents.net>
2025-09-16 06:25:02 -0700

    1

---
+++ Data.py
@@ -0,0 +1,22 @@
+#Basic Float Example
+# x = 3
+# y = float(3)
+# print(x,y)
+#what this code does is assign a value to "x" and "y"
+# when you put float in front of a number in parenthesis it makes it a decimal
+#then it prints x and prints y.
+
+# the output would be: 3 3.0
+#_______________________
+
+#lists
+# values = [1,2.23,5,7,2,30,15]
+# print(values)
+# for i in values:
+#     print(i)
+
+# print(values[0])
+# print(values[6])
+
+# print(values[7]) -This wouldnt work since there is no value "7" 
+#python starts counting at 0
---
+++ notes.py
@@ -0,0 +1,99 @@
+# #def is to define a function name and the parenthesis are parameteres or inputs.
+# #to run a function name it
+
+# #Loop example:
+# #for i in range(3):
+#     #print(i)
+
+# #Loops can be used in order to shorten code and reduce the amount of times-
+# #you actually have to write stuff. 
+
+
+
+# # logic:
+
+# # import turtle
+# # from turtle import * 
+# # t = Turtle()
+# # t.shape('turtle')
+# # def square(x):
+# #     for in range(4):
+# #         t.forward(x)
+# #         t.left(90)
+# # def doublesquare():
+# #     y = 100
+# #     for i in range(2):
+# #         square(y)
+
+# # doublesquare()
+# # turtle.done()
+
+
+
+
+# #Date types
+# # #strigns represent charachters or text.
+# # x = "dang it Yi"
+# #inputs output strings.
+# # name = input("whats's your name")
+# # print(name)
+# # def add(x,y):
+# #     return x + y
+# # z = add(5,15)
+# # print(z) 
+# # #could store values as variables and re use that.
+# # #integers or numbers 
+# # #have to add equivalent things
+# # a = ('5')
+# # print(a +5)
+# # #int for integer. You may
+# # #have to convert data types.
+# # # a = int('5')
+# # # bill = input("How much was the Bill?")
+# # # print(int(bill) * .15)
+
+# # name = "Mason"
+# # #use F string
+# # print(f"his name is {name}") 
+
+
+# #float = integer
+# #bill = 3.14
+
+# student = ["Cadee", "Mason", "Andy"]
+# student.append("Alina")
+
+# print(student[0])
+
+# for student in student:
+#    print(student)
+
+#    x = True
+#    y = False 
+
+
+#    if x == True:
+#       print("Hello Robby")
+# else:
+#    print("Goodbye Rodney")
+
+
+# x = 10
+# if x < 10 :
+#    print("less")
+
+# elif x == 10:
+#    print("equal")
+
+
+
+
+#____________________________________
+# DATA
+#Floats represent decimal numbers
+
+
+# x = ["This", "Sentance"]
+# print(len("This Sentance"))
+
+# counts letters including spaces.
